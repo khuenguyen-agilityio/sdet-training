e42efc4e14f22c3b6663fd92539cb818ef6f5883
Khue Nguyen <khue.nguyen@asnet.com.vn>
2024-02-21 14:22:28 +0700

    feat: add sample cucumber example 20240221

---
+++ src/test/java/hellocucumber/StepDefinitions.java
@@ -0,0 +1,38 @@
+package hellocucumber;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class IsItFriday {
+    static String isItFriday(String today) {
+        return "Friday".equals(today) ? "Sure" : "Nope";
+    }
+}
+
+public class StepDefinitions {
+    private String today;
+    private String actualAnswer;
+
+    @Given("today is Sunday")
+    public void today_is_sunday() {
+        today = "Sunday";
+    }
+
+    @When("I ask whether it's Friday yet")
+    public void i_ask_whether_it_s_friday_yet() {
+        actualAnswer = IsItFriday.isItFriday(today);
+    }
+
+    @Then("I should be told {string}")
+    public void i_should_be_told(String expectedAnswer) {
+        assertEquals(expectedAnswer, actualAnswer);
+    }
+
+    @Given("today is Friday")
+    public void today_is_Friday() {
+        today = "Friday";
+    }
+}
